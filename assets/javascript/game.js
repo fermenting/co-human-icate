43e455370201f56e6ca23d21e7da8c9a5016926d
Fermenting <realferalxanderyell@gmail.com>
2019-10-15 23:27:57 -0700

    stuck on words which have the same letter multiple times

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,76 +2,79 @@
 var wins = 0
 var losses = 0
 var chance = 100;
+var gameLetters = [];
 var wordBank = [
-  "computer", "information", "process", "program", "software", "hardware", "graphics", "error", "design"];
+  // "computerized", "information", "process", "program", "software", "hardware", "graphical", "error", "charger"];
+  "sssssss"];
+
 
 $(document).ready(function () {
-  console.log("ready!!");
-
   document.onkeyup = function () {
 
     // GAME START    
-    //  A word is selected from the array 
-    var gameWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+    function selectWord() {
+      //  A word is selected from the array and seperated into individual letters
+      var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+      gameLetters = gameWord.split("");
 
-    //  Word is seperated into individual letters
-
-    var gameLetters = gameWord.split("");
-
-    //iterate through the array of letters and create a div for each letter
-
-    for (var i = 0; i < gameLetters.length; i++) {
-
-      var blank = $("<p>");
-
-      blank.attr("class", "btn btn-lg btn-secondary unsolved");
-
-      blank.attr("id", gameLetters[i]);
-
-      blank.text(gameLetters[i]);
-
-      //Each letter is added by an opaque div
-      $("#mysteryWord").append(blank);
-
+      //iterate through the array of letters and create a div for each letter
+      for (var i = 0; i < gameLetters.length; i++) {
+        var blank = $("<p>");
+        blank.attr("class", "btn btn-lg btn-secondary unsolved");
+        blank.attr("id", gameLetters[i]);
+        blank.text(gameLetters[i]);
+        //Each letter is added to an opaque div
+        $("#mysteryWord").append(blank);
+      };
     }
-    // GAMEPLAY - We have to start logging key strokes now, otherwise the process will register the next keystroke to load up another word.
-
+    selectWord();
 
     // game is "listening" for guesses and judging them. 
+    document.onkeyup = function (type) {
 
-    document.onkeyup = function (blaze) {
+        if (type.key.replace(/[^a-z]/g, '')) {
+          var guess = type.key.toLowerCase();
+          playGame();
 
-      var guess = blaze.key.toLowerCase().replace(/[^a-z]/g,'');
-      console.log(guess)
-
-      for (i = 0; i < gameLetters.length; i++){
-        if (guess === gameLetters[i]) {
-          //use the id of the element to change the class to ".solved"
-          $("#"+guess).each(function(){$(this).removeClass("unsolved").addClass("solved")})
-
-          //check for unsolved elements. if no unsolved elements, game is won! Advance win counter by 1. Start new game!
-          // .hasClass("unsolved")
-        }
-
-        else {
-
-          //modify a visual element on the humanoid to show how progress...or regress.
-          //decrease chance counter. 
-       
-        }
-      }
-        $("#guessWrong").append(guess);
-        chance--
-        $("#chancesLeft").text(chance)
-        //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
-        if (chance === 0) {location.reload()}
-     
-      
+          // do {$("#" + guess).removeClass("unsolved").addClass("solved");}
+          // while ($("#" + guess).hasClass("unsolved")) 
+          function playGame() {
+            if ($("#" + guess).hasClass("unsolved")) {
+              $("#" + guess).removeClass("unsolved").addClass("solved");
+              playGame();
+            } else {
+              //modify a visual element on the humanoid to show how progress...or regress.
+              //decrease chance counter. 
+              $("#guessWrong").append(guess);
+              chance--
+              $("#chancesLeft").text(chance)
+              //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
+              if (chance === 0) { location.reload() }
+  
+            };
+          };
+          
+      };
     };
   };
 });
 
 
+function playGame() {
+  if ($("#" + guess).hasClass("unsolved")) {
+    $("#" + guess).removeClass("unsolved").addClass("solved");
+    playGame();
+  } else {
+    //modify a visual element on the humanoid to show how progress...or regress.
+    //decrease chance counter. 
+    $("#guessWrong").append(guess);
+    chance--
+    $("#chancesLeft").text(chance)
+    //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
+    if (chance === 0) { location.reload() }
+
+  };
+};
 //  Any old incorrect guesses are cleared
 //  Winner / Loser message is cleared
 
