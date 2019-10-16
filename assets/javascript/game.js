1366aaebc756c1d722e8ad6f122606b16c202869
Fermenting <realferalxanderyell@gmail.com>
2019-10-16 00:43:35 -0700

    wins & losses are correctly calculated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,17 @@
 // Initial state conditions
-var wins = 0
-var losses = 0
-var chance = 100;
+var wins = 0;
+var losses = 0;
+var chance = 5;
 var gameLetters = [];
+var uniqueLetters = 0;
 var wordBank = [
   "computerized", "information", "process", "program", "software", "hardware", "graphical", "error", "charger"];
-  // "sssssss"];
-
 
 $(document).ready(function () {
   document.onkeyup = function () {
 
     // GAME START    
-    function selectWord() {
-      //  A word is selected from the array and seperated into individual letters
-      var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-      gameLetters = gameWord.split("");
-
-      //iterate through the array of letters and create a div for each letter
-      for (var i = 0; i < gameLetters.length; i++) {
-        var blank = $("<p>");
-        blank.addClass("btn btn-lg btn-secondary unsolved " + gameLetters[i]);
-        blank.text(gameLetters[i]);
-        //Each letter is added to an opaque div
-        $("#mysteryWord").append(blank);
-      };
-    }
+    
     selectWord();
 
     // game is "listening" for guesses and judging them. 
@@ -33,18 +19,17 @@ $(document).ready(function () {
 
         if (type.key.replace(/[^a-z]/g, '')) {
           var guess = type.key.toLowerCase();
-          playGame();
-
-          // do {$("#" + guess).removeClass("unsolved").addClass("solved");}
-          // while ($("#" + guess).hasClass("unsolved")) 
+          playGame(); 
           function playGame() {
-            
-            // $("#mysteryWord").find("#" + guess).removeClass("unsolved").addClass("solved")
-
             if ($("." + guess).hasClass("unsolved")) {
-              // $("#mysteryWord").find("#" + guess).removeClass("unsolved").addClass("solved")
-
               $("." + guess).removeClass("unsolved").addClass("solved");
+              
+              uniqueLetters--;
+              if (uniqueLetters === 0){
+                wins++;
+                $("#wins").text(wins);
+                newGame();
+              }
             } else {
               //modify a visual element on the humanoid to show how progress...or regress.
               //decrease chance counter. 
@@ -52,40 +37,38 @@ $(document).ready(function () {
               chance--
               $("#chancesLeft").text(chance)
               //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
-              if (chance === 0) { location.reload() }
+              if (chance < 1) { 
+                losses++;
+                $("#losses").text(losses)
+                newGame();
+                }
   
             };
           };
-          
       };
     };
   };
 });
 
+function newGame (){
+  $("#mysteryWord").empty();
+  $("#guessWrong").empty();
+  selectWord();
+}
 
-function playGame() {
-  if ($("#" + guess).hasClass("unsolved")) {
-    $("#" + guess).removeClass("unsolved").addClass("solved");
-    playGame();
-  } else {
-    //modify a visual element on the humanoid to show how progress...or regress.
-    //decrease chance counter. 
-    $("#guessWrong").append(guess);
-    chance--
-    $("#chancesLeft").text(chance)
-    //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
-    if (chance === 0) { location.reload() }
-
+function selectWord() {
+  chance = 5
+  //  A word is selected from the array and seperated into individual letters
+  var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  gameLetters = gameWord.split("");
+  //count the number of unique letters
+  uniqueLetters = new Set(gameLetters).size
+  //iterate through the array of letters and create a div for each letter
+  for (var i = 0; i < gameLetters.length; i++) {
+    var blank = $("<p>");
+    blank.addClass("btn btn-lg btn-secondary unsolved " + gameLetters[i]);
+    blank.text(gameLetters[i]);
+    //Each letter is added to an opaque div
+    $("#mysteryWord").append(blank);
   };
-};
-//  Any old incorrect guesses are cleared
-//  Winner / Loser message is cleared
-
-// gameplay:
-// game is "listening" for guesses and judging them. 
-// correct = replace opque div with correct letter
-// incorrect = display guess, lower Chances by 1.
-
-// game is won if all letters are guessed correctly with chances left.
-//game is lost if all chances are used up and wordis not guessed.
-
+}
