339c18d3742c32c3b4a12eacf72760bcfea0d8a5
Fermenting <realferalxanderyell@gmail.com>
2019-10-16 19:18:30 -0700

    multiple guesses aren't repeating (good), too much anti-pattern (bad)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,63 +1,86 @@
+//TODO
+//do nothing when a mistake letter is repeated
+//make CSS better
+//fix hover cheat
+//readme
+//github pages
+//organize, DRY
+
 // Initial state conditions
 var wins = 0;
 var losses = 0;
-var chance = 5;
+var chance = 7;
 var gameLetters = [];
 var uniqueLetters = 0;
+var alreadyGuessed = {};
 var wordBank = [
-  "computerized", "information", "process", "program", "software", "hardware", "graphical", "error", "charger"];
+  "computerized", "information", "process", "program", "software", "hardware", "graphical", "error", "charger", "debugging", "networking"];
 
-$(document).ready(function () {
-  document.onkeyup = function () {
+selectWord();
+keyListen();
 
-    // GAME START    
-    
-    selectWord();
-
-    // game is "listening" for guesses and judging them. 
+function keyListen() {
+  $(document).ready(function () {
     document.onkeyup = function (type) {
 
-        if (type.key.replace(/[^a-z]/g, '')) {
-          var guess = type.key.toLowerCase();
-          playGame(); 
-          function playGame() {
-            if ($("." + guess).hasClass("unsolved")) {
-              $("." + guess).removeClass("unsolved").addClass("solved");
-              
-              uniqueLetters--;
-              if (uniqueLetters === 0){
-                wins++;
-                $("#wins").text(wins);
-                newGame();
-              }
-            } else {
-              //modify a visual element on the humanoid to show how progress...or regress.
-              //decrease chance counter. 
-              $("#guessWrong").append(guess);
-              chance--
-              $("#chancesLeft").text(chance)
-              //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
-              if (chance < 1) { 
-                losses++;
-                $("#losses").text(losses)
-                newGame();
-                }
-  
-            };
-          };
-      };
-    };
-  };
-});
 
-function newGame (){
+      // GAME START    
+      var guess = type.key.replace(/[^A-Za-z]+$/g, '').toLowerCase()
+      console.log("guess " + guess)
+
+
+      // if (type.key.replace(/[^a-z]/g, '')) {
+      //   var guess = type.key.toLowerCase();
+      //   console.log(guess)
+      if (!alreadyGuessed[guess]) {
+        alreadyGuessed[guess] = true;
+
+        playGame();
+        
+
+        function playGame() {
+          if ($("." + guess).hasClass("unsolved")) {
+            $("." + guess).removeClass("unsolved").addClass("solved");
+        
+            uniqueLetters--;
+            if (uniqueLetters === 0) {
+              wins++;
+              $("#wins").text(wins);
+              newGame();
+            }
+          } else {
+            //modify a visual element on the humanoid to show how progress...or regress.
+            //decrease chance counter. 
+            $("#guessWrong").append(guess);
+            chance--
+            $("#chancesLeft").text(chance)
+            //if counter reaches 0, game over. Advance Loss counter by 1. Start new game!
+            if (chance < 1) {
+              losses++;
+              $("#losses").text(losses)
+              newGame();
+            };
+          }
+        };
+
+
+      } else {
+        return
+      }
+    };
+  });
+};
+
+function newGame() {
   $("#mysteryWord").empty();
   $("#guessWrong").empty();
+  alreadyGuessed = {};
   selectWord();
 }
 
 function selectWord() {
-  chance = 5
+  chance = 7
+  $("#chancesLeft").text(chance)
   //  A word is selected from the array and seperated into individual letters
   var gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
   gameLetters = gameWord.split("");
@@ -72,3 +95,22 @@ function selectWord() {
     $("#mysteryWord").append(blank);
   };
 }
+
+
+
+// var already = false;
+//         for (var i = 0; i < alreadyGuessed.length; i++) {
+//           if (guess === alreadyGuessed[i]) {
+//             already = true
+//           }
+//           if (already) {
+//             already = false;
+//             return
+//           }
+//           else {
+
+
+
+  // switch (guess) {
+  //   case 
+  // }
